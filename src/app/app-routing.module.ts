07f87eee82f4115e107010597c403b8e8f7d53f4
PierreD <pierredemailly.pro@gmail.com>
2020-08-27 15:13:36 +0200

    add sidenav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HandGridComponent } from './shared/components/hand-grid/hand-grid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '',
+    pathMatch: 'full',
+    component: HandGridComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,32 @@
 import { MouseService } from './shared/services/mouse/mouse.service';
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  @HostListener('mousedown', ['$event']) onMouseDown(event: MouseEvent): void {
+export class AppComponent implements OnInit {
+  isMobile: boolean;
+  isTablet: boolean;
+  isDesktop: boolean;
+
+  @HostListener('mousedown', ['$event']) onMouseDown(): void {
     this.mouseService.mouseDown();
   }
   @HostListener('mouseup', ['$event']) onMouseUp(): void {
     this.mouseService.mouseUp();
   }
 
-  constructor(private mouseService: MouseService) {}
+  constructor(
+    private mouseService: MouseService,
+    private deviceService: DeviceDetectorService,
+  ) {}
+
+  ngOnInit(): void {
+    this.isMobile = this.deviceService.isMobile();
+    this.isTablet = this.deviceService.isTablet();
+    this.isDesktop = this.deviceService.isDesktop();
+  }
 }
